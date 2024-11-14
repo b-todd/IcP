719f2ee811a011c4f035473735b3a84475ddf67d
Ben Todd <149726677+b-todd@users.noreply.github.com>
2024-11-14 13:04:32 -0600

    Fixing Nav
    Clicking nav will toggle it on or off.

--- interactions.js
+++ interactions.js
@@ -19,32 +19,53 @@ function openTab(evt, tabName) {
   evt.currentTarget.className += " active";
 }
 
+var delayInMilliseconds = 2000; //1 second
 
-//Making a function that will toggle the menu based on the device width
-function myFunction(x) {
-     if (x.matches) { // If media query matches
-       document.body.style.backgroundColor = "yellow";
-       closeNav();
-     } else {
-       document.body.style.backgroundColor = "pink";
-       closeNav();
-     }
-   }
-   
-   // Create a MediaQueryList object
-   var x = window.matchMedia("(max-width: 700px)")
-   
-   // Call listener function at run time
-   myFunction(x);
-   
-   // Attach listener function on state changes
-   x.addEventListener("change", function() {
-     myFunction(x);
-   });
+setTimeout(function() {
+  //your code to be executed after 1 second
+}, delayInMilliseconds);
 
+function toggleNav() {
+  let navStatus = document.getElementById("Sidenav").className;
+  console.log(navStatus)
 
+  if (navStatus == "close") {
+    console.log('we are going to open the nav');
+    openNav();
+    //document.getElementById("Sidenav").onclick = null;
+  } else {
+    console.log('we are going to close the nav');
+    closeNav();
+    //document.getElementById("Sidenav").onclick = "toggleNav()";  
+    //if (document.getElementById("Sidenav").onclick == null) {
+    //  console.log('the nav does not have and onclick applied')
+    //} else {
+    //  console.log('onclick is still present?')
+    //}
+  }
+}
 
+function openNav() {
+  console.log('opennav has been initiated');
+  document.getElementById("Sidenav").classList.remove("close");
+  document.body.style.marginLeft = "320px";
+}
+function closeNav() {
+  console.log('closenav has been initiated');
+  document.getElementById("Sidenav").classList.add("close");
+  document.body.style.marginLeft = "60px";
+}
 
+/*
+  Sidenavwidth = document.getElementById("Sidenav").style.width;
+  if (Sidenavwidth >= 120) {
+        closeNav();
+  } else {
+        openNav();
+  }
+}
+
+/*
 function toggleNav(Sidenav) {
      var Sidenavwidth;
 
@@ -63,10 +84,34 @@ function openNav() {
      document.getElementById("navtitle").style.display = "flex";
    }
    
-   /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+   /* Set the width of the side navigation to 0 and the left margin of the page content to 0 
    function closeNav() {
      document.getElementById("Sidenav").style.width = "0";
      document.body.style.marginLeft = "50px";
 
      document.getElementById("main").style.marginLeft = "50px";
    }
+
+   //Making a function that will toggle the menu based on the device width
+function myFunction(x) {
+  if (x.matches) { // If media query matches
+    document.body.style.backgroundColor = "yellow";
+    closeNav();
+  } else {
+    document.body.style.backgroundColor = "pink";
+    closeNav();
+  }
+}
+
+// Create a MediaQueryList object
+var x = window.matchMedia("(max-width: 700px)")
+
+// Call listener function at run time
+myFunction(x);
+
+// Attach listener function on state changes
+x.addEventListener("change", function() {
+  myFunction(x);
+});
+
+*/
