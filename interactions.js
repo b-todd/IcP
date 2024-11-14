b847d47b4208ee42c0479f6837f45729e3bf579f
Ben Todd <149726677+b-todd@users.noreply.github.com>
2024-11-14 13:13:03 -0600

    Learning to push
    removed some code. Nav should open/close now.

--- interactions.js
+++ interactions.js
@@ -19,11 +19,6 @@ function openTab(evt, tabName) {
   evt.currentTarget.className += " active";
 }
 
-var delayInMilliseconds = 2000; //1 second
-
-setTimeout(function() {
-  //your code to be executed after 1 second
-}, delayInMilliseconds);
 
 function toggleNav() {
   let navStatus = document.getElementById("Sidenav").className;
@@ -36,12 +31,6 @@ function toggleNav() {
   } else {
     console.log('we are going to close the nav');
     closeNav();
-    //document.getElementById("Sidenav").onclick = "toggleNav()";  
-    //if (document.getElementById("Sidenav").onclick == null) {
-    //  console.log('the nav does not have and onclick applied')
-    //} else {
-    //  console.log('onclick is still present?')
-    //}
   }
 }
 
@@ -55,63 +44,3 @@ function closeNav() {
   document.getElementById("Sidenav").classList.add("close");
   document.body.style.marginLeft = "60px";
 }
-
-/*
-  Sidenavwidth = document.getElementById("Sidenav").style.width;
-  if (Sidenavwidth >= 120) {
-        closeNav();
-  } else {
-        openNav();
-  }
-}
-
-/*
-function toggleNav(Sidenav) {
-     var Sidenavwidth;
-
-     Sidenavwidth = document.getElementById("Sidenav").style.width;
-     if (Sidenavwidth >= 120) {
-          closeNav();
-     } else {
-          openNav();
-     }
-}
-
-function openNav() {
-     document.getElementById("Sidenav").style.width = "320px";
-     document.body.style.marginLeft = "320px";
-
-     document.getElementById("navtitle").style.display = "flex";
-   }
-   
-   /* Set the width of the side navigation to 0 and the left margin of the page content to 0 
-   function closeNav() {
-     document.getElementById("Sidenav").style.width = "0";
-     document.body.style.marginLeft = "50px";
-
-     document.getElementById("main").style.marginLeft = "50px";
-   }
-
-   //Making a function that will toggle the menu based on the device width
-function myFunction(x) {
-  if (x.matches) { // If media query matches
-    document.body.style.backgroundColor = "yellow";
-    closeNav();
-  } else {
-    document.body.style.backgroundColor = "pink";
-    closeNav();
-  }
-}
-
-// Create a MediaQueryList object
-var x = window.matchMedia("(max-width: 700px)")
-
-// Call listener function at run time
-myFunction(x);
-
-// Attach listener function on state changes
-x.addEventListener("change", function() {
-  myFunction(x);
-});
-
-*/
